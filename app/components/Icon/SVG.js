e2865beda7ef237ad790fd858574f95ba846db91
tmfrnz <timo@dumpark.com>
2017-03-01 11:47:18 +1300

    adding preliminary icon component

---
+++ app/components/Icon/SVG.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+import { palette } from 'styled-theme';
+
+const SVG = styled.svg`
+  fill: ${ props => props.palette ? palette(props.paletteIndex) : 'currentColor' };
+  width: ${ props => props.size };
+  height: ${ props => props.size };
+  display: inline-flex;
+  align-self: center;
+  position:relative;
+  bottom: ${ props => parseFloat(props.size)/16 + props.size.split(/[0-9]+/)[1] };
+  margin-right:${ props => parseFloat(props.size)/4 + props.size.split(/[0-9]+/)[1] };
+`
+
+export default SVG;
--- app/components/Icon/index.js
+++ app/components/Icon/index.js
@@ -1,45 +1,37 @@
 import React from 'react';
-import icons from 'themes/icons.js';
-import styled from 'styled-components';
-import { palette } from 'styled-theme';
+import icons from 'themes/icons';
+
+import SVG from './SVG'
 
 function Icon(props) {
   // var SVG = IconFactory(icons)
-  let size = props.size || 24
-  let name = typeof props.name !== "undefined" && typeof icons[props.name] !== "undefined" ? props.name  : 'fallback'
 
-  const Wrapper = styled.span`
-    padding: 0;
-    margin: 0;
-  `;
-
-  const SVG = styled.svg`
-    vertical-align:middle;
-    fill: ${palette(1)};
-    width: ${ size + 'px'};
-    height: ${ size + 'px' };
-  `
-  SVG.defaultProps = {
-    palette: 'primary'
-  }
-
-  return (
-    <Wrapper className={props.className} >
+  if (typeof props.name !== "undefined"  && typeof icons[props.name] !== "undefined"){
+    return (
       <SVG
-        viewBox="0 0 24 24"
+        viewBox="0 0 1024 1024"
         preserveAspectRatio="xMidYMid meet"
         palette={props.palette}
+        paletteIndex={props.paletteIndex || 0}
+        size={props.size || "1em"}
       >
-        {icons[name]}
+        {
+          icons[props.name].map(function(path, index){
+            return <path d={path} key={index}></path>;
+          })
+        }
       </SVG>
-    </Wrapper>
-  );
+    );
+  } else {
+    return null
+  }
 }
 
 Icon.propTypes = {
   name: React.PropTypes.string,
   palette: React.PropTypes.string,
-  size: React.PropTypes.number
+  paletteIndex: React.PropTypes.number,
+  size: React.PropTypes.string
 };
 
 
