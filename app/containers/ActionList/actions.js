d8fcd0ceb9fc4c057c46d69a749c75c72e8f6a32
Tim Field <tim@thefold.co.nz>
2017-03-10 16:35:43 +1300

    remove unused code in action list

--- app/containers/ActionList/actions.js
+++ app/containers/ActionList/actions.js
@@ -3,15 +3,3 @@
  * ActionList actions
  *
  */
-
-import {
-  SET_SORT,
-} from './constants';
-
-export function setSort(sort, order) {
-  return {
-    type: SET_SORT,
-    sort,
-    order,
-  };
-}
--- app/containers/ActionList/reducer.js
+++ app/containers/ActionList/reducer.js
@@ -5,23 +5,12 @@
  */
 
 import { fromJS } from 'immutable';
-import {
-  SET_SORT,
-} from './constants';
 
 const initialState = fromJS({
-  sort: 'id',
-  order: 'desc',
-  perPage: 5,
-  currentPage: 1,
 });
 
 function actionListReducer(state = initialState, action) {
   switch (action.type) {
-    case SET_SORT:
-      return state
-        .set('sort', action.sort)
-        .set('order', action.order);
     default:
       return state;
   }
