382901c7654c3207c046cce9c059f4e91256a79c
tmfrnz <timo@dumpark.com>
2017-03-19 16:57:25 +1300

    taxonomy-category linked list, also allow multiple getEntity-extends

---
+++ app/containers/TaxonomyCategories/messages.js
@@ -0,0 +1,29 @@
+/*
+ * Taxonomies Messages
+ *
+ * This contains all the text for the Taxonomies component.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  pageTitle: {
+    id: 'app.containers.TaxonomyCategories.pageTitle',
+    defaultMessage: 'Categories',
+  },
+  metaDescription: {
+    id: 'app.containers.TaxonomyCategories.metaDescription',
+    defaultMessage: 'Categories overview',
+  },
+  header: {
+    id: 'app.containers.TaxonomyCategories.header',
+    defaultMessage: 'Category overview',
+  },
+  notFound: {
+    id: 'app.containers.TaxonomyCategories.notFound',
+    defaultMessage: 'Sorry no categories found',
+  },
+  loading: {
+    id: 'app.containers.TaxonomyCategories.loading',
+    defaultMessage: 'Loading categories...',
+  },
+});
