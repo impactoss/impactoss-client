972a40e3f6405ec16048b31e8520d9f9c0386dfd
Tim Field <tim@thefold.co.nz>
2017-04-06 13:48:02 +1200

    check filter values on page load

--- app/components/FilterForm/index.js
+++ app/components/FilterForm/index.js
@@ -1,9 +1,12 @@
 import React, { PropTypes } from 'react';
+import { browserHistory } from 'react-router';
 import Immutable from 'immutable';
-import { Form } from 'react-redux-form/immutable';
+import { connect } from 'react-redux';
+import { Form, actions as formActions } from 'react-redux-form/immutable';
 import MultiSelect from 'components/MultiSelect';
 
-export default class FilterForm extends React.Component { // eslint-disable-line react/prefer-stateless-function
+// TODO as this now connects to redux in order to populate the form maybe it should live in `containers`
+class FilterForm extends React.Component { // eslint-disable-line react/prefer-stateless-function
 
   static propTypes = {
     model: PropTypes.string.isRequired,
@@ -11,6 +14,16 @@ export default class FilterForm extends React.Component { // eslint-disable-line
     handleSubmit: PropTypes.func,
     onClose: PropTypes.func,
     title: PropTypes.string,
+    populateForm: PropTypes.func.isRequired,
+  }
+
+  constructor(props) {
+    super(props);
+    this.URLParams = new URLSearchParams(browserHistory.getCurrentLocation().search);
+  }
+
+  componentWillMount() {
+    this.props.populateForm(this.props.model, this.props.options, this.URLParams);
   }
 
   render() {
@@ -28,12 +41,31 @@ export default class FilterForm extends React.Component { // eslint-disable-line
         <MultiSelect
           model=".values"
           options={this.props.options}
-          valueCompare={(a, b) =>
+          valueCompare={(value, option) =>
             // our values, are maps with nested value keys :)
-            a.get('value') === b.get('value')
+            value.get('value') === option.get('value')
           }
         />
       </Form>
     );
   }
 }
+
+const mapDispatchToProps = (dispatch) => ({
+  populateForm: (model, options, URLParams) => {
+    // Filter each option, keep the values of those in the URL
+    const urlValues = options
+    .filter((option) => {
+      const query = option.getIn(['value', 'query']);
+      const value = option.getIn(['value', 'value']);
+      return URLParams.has(query) && URLParams.getAll(query).indexOf(value.toString()) >= 0;
+    })
+    .map((option) => option.get('value'));
+
+    if (urlValues.size > 0) {
+      dispatch(formActions.load(model, Immutable.Map({ values: urlValues })));
+    }
+  },
+});
+
+export default connect(null, mapDispatchToProps)(FilterForm);
