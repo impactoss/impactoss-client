0a79c96e83ddfc33295a7df3240732aedbcbb331
tmfrnz <timo@dumpark.com>
2017-06-19 19:22:01 +1200

    revert brand title

--- app/components/Header/BrandTitle.js
+++ app/components/Header/BrandTitle.js
@@ -3,6 +3,6 @@ import styled from 'styled-components';
 export default styled.h1`
   font-family: ${(props) => props.theme.fonts.headerBrandMain};
   font-size: ${(props) => props.theme.sizes.headerBrandMain};
+  text-transform: uppercase;
   margin: 0;
 `;
-// text-transform: uppercase;
