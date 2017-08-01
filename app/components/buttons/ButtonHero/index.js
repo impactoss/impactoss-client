6d691b772b77a1f6070bd2d0e2c6ce16dbf1f643
tmfrnz <timo@dumpark.com>
2017-08-01 18:35:14 +1200

    cleanup homepage #11

--- app/components/buttons/ButtonHero/index.js
+++ app/components/buttons/ButtonHero/index.js
@@ -1,8 +1,15 @@
 import styled from 'styled-components';
+import { palette } from 'styled-theme';
 import ButtonDefault from '../ButtonDefault';
 
 const ButtonHero = styled(ButtonDefault)`
   font-size: 1.25em;
+  padding: 0.8em;
+  min-width: 200px;
+  background-color: ${palette('secondary', 0)};
+  &:hover {
+    background-color: ${palette('secondary', 1)};
+  }
 `;
 
 export default ButtonHero;
