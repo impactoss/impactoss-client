5c6b3fc9bec8357e6c99ac5efde45e2d1d2d6eed
tmfrnz <timo@dumpark.com>
2017-12-02 16:22:00 +0100

    required asterisk #402

--- app/components/forms/Required.js
+++ app/components/forms/Required.js
@@ -4,10 +4,8 @@ import { palette } from 'styled-theme';
 const Required = styled.span`
   color: ${palette('primary', 1)};
   font-weight: bold;
-  font-size: 2em;
-  padding-left: 5px;
-  position: absolute;
-  top: -0.25em;
+  font-size: 1.3em;
+  padding-left: 3px;
 `;
 
 export default Required;
