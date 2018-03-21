0fdfa0a1b267436639d47a0b235d64fee189c773
dmprk <edith@dumpark.com>
2018-03-21 15:35:40 +1300

    update theme colours

--- app/themes/theme-nz.js
+++ app/themes/theme-nz.js
@@ -95,8 +95,8 @@ const light = coolorsToHex('https://coolors.co/f5f6f6-eaeced-dbdfe0-bbc1c2-aeb6b
 // other palettes
 // 0: AA on light[0] and on error[3]
 // 1: AA on white
-const error = coolorsToHex('https://coolors.co/b20e0e-c10f0f-d31717-f2e3e3-ffffff');
-const success = coolorsToHex('https://coolors.co/00632e-007034-007c3a-e1f2ed-ffffff');
+const error = coolorsToHex('https://coolors.co/a41d23-b22026-c3292e-f1e4e4-ffffff');
+const success = coolorsToHex('https://coolors.co/048e47-099b50-07aa51-09b757-ffffff');
 // const alert = coolorsToHex('https://coolors.co/c75300-e56700-ed7000-f97807-ffffff');
 // const info = coolorsToHex('https://coolors.co/0063b5-0070cc-0077d8-118ef4-ffffff');
 
@@ -126,27 +126,27 @@ theme.palette = {
   // taxonomiesHoverAAL: ['#6D787E', '#3A1D49', '#3B3681', '#28865D', '#21827B', '#3D832F', '#003A5C', '#147CA9', '#6D787E'],
   taxonomies: [
     '#E8EAEB', // 0: default/fallback
-    '#6444AF', // 1: Human Rights Body
-    '#88338E', // 2: UN session
-    '#416680', // 3: Country
+    '#0059A3', // 1: Human Rights Body
+    '#0077D8', // 2: UN session
+    '#687E8D', // 3: Country
     '#009ED8', // 4: SDGs
-    '#00A088', // 5: Issue
-    '#00C4B3', // 6: Population group
-    '#099B50', // 7: SMART // '#E56700',
-    '#01527A', // 8: Agency
-    '#01527A', // unused
+    '#7A52A3', // 5: Issue
+    '#BE4180', // 6: Population group
+    '#D97720', // 7: SMART // '#E56700',
+    '#0D5273', // 8: Agency
+    '#383838', // unused
   ],
   taxonomiesHover: [
     '#DBDCDD', // 0: default/fallback
-    '#523890', // 1: Human Rights Body
-    '#924598', // 2: UN session
-    '#395970', // 3: Country
+    '#005296', // 1: Human Rights Body
+    '#0070CC', // 2: UN session
+    '#596B78', // 3: Country
     '#007FAD', // 4: SDGs
-    '#00927C', // 5: Issue
-    '#00B3A3', // 6: Population group
-    '#048E47', // 7: SMART // '#C75300',
-    '#014A6F', // 8: Agency
-    '#014A6F', // unused
+    '#704B95', // 5: Issue
+    '#AF3C75', // 6: Population group
+    '#B35F00', // 7: SMART // '#C75300',
+    '#0B4560', // 8: Agency
+    '#383838', // unused
   ],
   // bg inactive, bg hover, icon
   smartInactive: [
@@ -158,14 +158,14 @@ theme.palette = {
   // other entities
   // [aqll #AA compliant]
   // maybe [#AA-Large compliant] 18pt/24px or 14pt/19px bold can suffice with AA com,pliant hover if agreed by customer
-  measuresHover: ['#C75300'], // ['#E3B707'],
-  measures: ['#E56700'], // ['#F9C907'],
-  recommendationsHover: ['#026B9F', '#03405F'], // accepted, noted ['#FF9B69', '#FFB28B'],
-  recommendations: ['#027DBB', '#045984'],
+  measuresHover: ['#048E47'], // ['#E3B707'],
+  measures: ['#099B50'], // ['#F9C907'],
+  recommendationsHover: ['#0B537A', '#126FA1'], // accepted, noted ['#FF9B69', '#03405F'],
+  recommendations: ['#0D608E', '#147EB9'], // accepted, noted ['#FF9B69', '#045984'],
   sdgtargetsHover: ['#007FAD'],
-  sdgtargets: ['#009ED8'],
-  indicatorsHover: ['#C81438'],
-  indicators: ['#DB153D'],
+  sdgtargets: ['#009ED8'], // 40D7FF, 00A0CC
+  indicatorsHover: ['#06562D'],
+  indicators: ['#0A6B39'],
   reportsHover: [error[1]],
   reports: [error[0]],
   attributesHover: ['#47545B'],
