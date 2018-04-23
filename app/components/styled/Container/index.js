cb5e903c0c12e2b95ebaada3a65e4b2aa703b6fe
tmfrnz <timo@dumpark.com>
2018-04-23 15:50:38 +1200

    #447 small: container horizontal padding

--- app/components/styled/Container/index.js
+++ app/components/styled/Container/index.js
@@ -4,8 +4,13 @@ import { palette } from 'styled-theme';
 const Container = styled.div`
   margin-right: auto;
   margin-left: auto;
-  max-width: ${(props) => props.isNarrow ? '960px' : '1170px'};
+  max-width: 100%;
   padding-bottom: ${(props) => props.noPaddingBottom || props.inModal ? 0 : '3em'};
   background-color: ${(props) => props.inModal ? palette('light', 0) : 'transparent'};
+  padding-right: 12px;
+  padding-left: 12px;
+  @media (min-width: ${(props) => props.theme.breakpoints.large}) {    
+    max-width: ${(props) => props.isNarrow ? '960' : (props.theme.breakpoints.large - 30)}px;
+  }
 `;
 export default Container;
