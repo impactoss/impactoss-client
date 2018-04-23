4f5906e4e4895097668350ba6a1d76b9d825dcf3
tmfrnz <timo@dumpark.com>
2018-04-24 10:38:16 +1200

    #447 small: hero button

--- app/components/buttons/ButtonHero/index.js
+++ app/components/buttons/ButtonHero/index.js
@@ -2,9 +2,13 @@ import styled from 'styled-components';
 import ButtonDefault from '../ButtonDefault';
 
 const ButtonHero = styled(ButtonDefault)`
-  font-size: 1.25em;
+  font-size: 1.1em;
   padding: 0.8em;
-  min-width: 200px;
+  min-width: 160px;
+  @media (min-width: ${(props) => props.theme.breakpoints.large}) {
+    font-size: 1.25em;
+    min-width: 200px;
+  }
 `;
 
 export default ButtonHero;
