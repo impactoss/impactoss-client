910a7665cbbb38bd5b90480b92740b3272ff9231
tmfrnz <timo@dumpark.com>
2018-05-01 19:24:49 +1200

    #447 mobile: secondary nav icons WIP

--- app/components/Header/LinkSecondary.js
+++ app/components/Header/LinkSecondary.js
@@ -7,7 +7,7 @@ export default styled(Link)`
   font-size: 1em;
   display: block;
   border-bottom: 1px solid #fff;
-  border-color: ${(props) => palette('header', 0)};
+  border-color: ${palette('header', 0)};
   @media (min-width: ${(props) => props.theme.breakpoints.small}) {
     border-bottom: none;
     padding-top: 0.4em;
