0e5d6b1fe4c5c7a6bc5c6546917b963abb52717c
tmfrnz <timo@dumpark.com>
2021-01-11 17:13:30 +0100

    use staging database

--- app/themes/config.js
+++ app/themes/config.js
@@ -78,12 +78,11 @@ export const CYCLE_TAXONOMY_ID = 2;
 
 // General ********************
 
-// ? 'https://undp-sadata-staging.herokuapp.com'
 export const ENDPOINTS = {
   API: (
     NODE_ENV === 'production'
-    ? 'https://impactoss-dev.herokuapp.com/'
-    : 'https://impactoss-dev.herokuapp.com/'
+    ? 'https://undp-sadata-staging.herokuapp.com/'
+    : 'https://undp-sadata-staging.herokuapp.com/'
   ), // server API endpoint
   SIGNING_URL: '/s3/sign', // server AWS S3 signing url endpoint
   SIGN_IN: 'auth/sign_in',
