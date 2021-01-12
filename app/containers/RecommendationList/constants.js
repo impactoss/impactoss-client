75ad65140606ba5b3c2c9454f8e5ddedc87c1e1d
tmfrnz <timo@dumpark.com>
2021-01-12 17:54:09 +0100

    order recs by ref

--- app/containers/RecommendationList/constants.js
+++ app/containers/RecommendationList/constants.js
@@ -25,12 +25,12 @@ export const CONFIG = {
       attribute: 'id', // proxy for created at
       type: 'number',
       order: 'desc',
-      default: true,
     },
     {
       attribute: 'reference',
       type: 'string',
       order: 'asc',
+      default: true,
     },
     {
       attribute: 'title',
