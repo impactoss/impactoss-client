5e5acc3f7ab663cc3e0d13cc9ccd8b9ebd0ccd9f
tmfrnz <timo@dumpark.com>
2021-01-18 18:42:20 +0100

    upgrade boilerplate WIP

--- app/i18n.js
+++ app/i18n.js
@@ -3,6 +3,9 @@
  *
  * This will setup the i18n language files and locale data for your app.
  *
+ *   IMPORTANT: This file is used by the internal build
+ *   script `extract-intl`, and must use CommonJS module syntax
+ *   You CANNOT use import/export in this file.
  */
 const addLocaleData = require('react-intl').addLocaleData; //eslint-disable-line
 const enLocaleData = require('react-intl/locale-data/en');
@@ -11,6 +14,7 @@ const enTranslationMessages = require('./translations/en-GB.json');
 
 const DEFAULT_LOCALE = 'en-GB';
 
+// prettier-ignore
 const appLocales = [
   'en-GB',
 ];
@@ -18,13 +22,15 @@ const appLocales = [
 addLocaleData(enLocaleData);
 
 const formatTranslationMessages = (locale, messages) => {
-  const defaultFormattedMessages = locale !== DEFAULT_LOCALE
-    ? formatTranslationMessages(DEFAULT_LOCALE, enTranslationMessages)
-    : {};
+  const defaultFormattedMessages =
+    locale !== DEFAULT_LOCALE
+      ? formatTranslationMessages(DEFAULT_LOCALE, enTranslationMessages)
+      : {};
   const flattenFormattedMessages = (formattedMessages, key) => {
-    const formattedMessage = !messages[key] && locale !== DEFAULT_LOCALE
-      ? defaultFormattedMessages[key]
-      : messages[key];
+    const formattedMessage =
+      !messages[key] && locale !== DEFAULT_LOCALE
+        ? defaultFormattedMessages[key]
+        : messages[key];
     return Object.assign(formattedMessages, { [key]: formattedMessage });
   };
   return Object.keys(messages).reduce(flattenFormattedMessages, {});
