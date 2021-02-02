46f7394da467da1ee362d78978c73e8866bc89ca
tmfrnz <timo@dumpark.com>
2021-02-02 18:53:34 +0100

    fix accidently hidden list, print WIP

--- app/containers/EntityList/index.js
+++ app/containers/EntityList/index.js
@@ -19,7 +19,6 @@ import Loading from 'components/Loading';
 import EntityListSidebar from 'components/EntityListSidebar';
 import EntityListSidebarLoading from 'components/EntityListSidebarLoading';
 import EntityListMain from 'components/EntityListMain';
-import PrintHide from 'components/styled/PrintHide';
 
 import {
   selectHasUserRole,
@@ -146,7 +145,7 @@ export class EntityList extends React.PureComponent { // eslint-disable-line rea
       : entityIdsSelected;
 
     return (
-      <PrintHide>
+      <div>
         { !this.props.dataReady
           && <EntityListSidebarLoading />
         }
@@ -295,7 +294,7 @@ export class EntityList extends React.PureComponent { // eslint-disable-line rea
             </Progress>
           )
         }
-      </PrintHide>
+      </div>
     );
   }
 }
