6dbbe1cc1a4455dc775d3389be3d08ecfcc10780
vbojilova <viki.bojilova@gmail.com>
2024-02-15 16:10:51 +1300

    changed checkbox coloring - #533

--- app/components/forms/IndeterminateCheckbox/index.js
+++ app/components/forms/IndeterminateCheckbox/index.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import styled from 'styled-components';
+import { palette } from 'styled-theme';
+
 export const STATES = {
   INDETERMINATE: null,
   CHECKED: true,
   UNCHECKED: false,
 };
 
+const Input = styled.input`
+accent-color: ${palette('checkbox', 0)};
+`;
 export default class IndeterminateCheckbox extends React.Component {
   static propTypes = {
     checked: PropTypes.oneOf(Object.values(STATES)),
     onChange: PropTypes.func.isRequired,
-  }
+  };
 
   render() {
     const { onChange, checked, ...props } = this.props;
     /* eslint-disable no-param-reassign */
     return (
-      <input
+      <Input
         type="checkbox"
         ref={(ref) => { if (ref) ref.indeterminate = checked === STATES.INDETERMINATE; }}
         checked={!!checked}
