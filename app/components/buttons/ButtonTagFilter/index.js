074533386f59edb0f27f3e823865536a9d29ec85
tmfrnz <timo@dumpark.com>
2024-03-19 16:30:04 +1300

    improve tag search (3)

--- app/components/buttons/ButtonTagFilter/index.js
+++ app/components/buttons/ButtonTagFilter/index.js
@@ -13,7 +13,7 @@ const ButtonTagFilter = styled(Button)`
   color: ${({ pIndex }) => getTextColor(pIndex)};
   background-color: ${(props) => palette(props.palette, props.pIndex || 0)};
   padding: 1px 6px;
-  margin-right: 2px;
+  margin: 1px;
   border-radius: 3px;
   font-size: 0.85em;
   border: 1px solid ${(props) => palette(props.palette, props.pIndex || 0)};
