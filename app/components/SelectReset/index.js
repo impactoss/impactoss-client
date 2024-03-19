d270db44955a06f26e63eee760fb574c9880d0df
tmfrnz <timo@dumpark.com>
2024-03-19 20:05:56 +1300

    improve SelectReset #25

--- app/components/SelectReset/index.js
+++ app/components/SelectReset/index.js
@@ -10,8 +10,11 @@ import { find } from 'lodash/collection';
 import styled from 'styled-components';
 import { palette } from 'styled-theme';
 
+import ScreenReaderHide from 'components/styled/ScreenReaderHide';
 import ButtonSimple from 'components/buttons/ButtonSimple';
 import Icon from 'components/Icon';
+
+import messages from './messages';
 
 const Label = styled.label`
   color: ${palette('text', 1)};
@@ -53,7 +56,7 @@ const Reset = styled(ButtonSimple)`
   vertical-align: middle;
   color: ${palette('link', 2)};
   margin-right: 20px;
-  font-weight: 500; 
+  font-weight: 500;
 
   &:hover, &:focus-visible {
     color: ${palette('linkHover', 2)};
@@ -79,38 +82,43 @@ export class SelectReset extends React.PureComponent { // eslint-disable-line re
       index,
       hidePrint,
     } = this.props;
+    const { intl } = this.context;
     const optionActive = find(options, (option) => option.value === value);
     return (
       <Styled hidePrint={hidePrint}>
         {label
           && <Label htmlFor={index}>{label}</Label>
         }
-        {(!isReset || optionActive.value === emptyValue)
-          && (
-            <Select
-              id={index}
-              onChange={(event) => onChange(event.target.value)}
-              value={value}
-              active={false}
-            >
-              {options.map((option, i) => (
-                <Option
-                  key={i}
-                  value={option.value}
-                  isPlaceholder={option.value === emptyValue}
-                  default={option.default}
-                  active={option.value === value}
-                >
-                  {option.label}
-                </Option>
-              ))}
-            </Select>
-          )
-        }
+        {(!isReset || optionActive.value === emptyValue) && (
+          <Select
+            id={index}
+            onChange={(event) => onChange(event.target.value)}
+            value={value}
+            active={false}
+          >
+            {options.map((option, i) => (
+              <Option
+                key={i}
+                value={option.value}
+                isPlaceholder={option.value === emptyValue}
+                default={option.default}
+                active={option.value === value}
+              >
+                {option.label}
+              </Option>
+            ))}
+          </Select>
+        )}
         {isReset && optionActive.value !== emptyValue
           && (
-            <Reset onClick={() => onChange(emptyValue)}>
-              {optionActive.label}
+            <Reset
+              id={index}
+              onClick={() => onChange(emptyValue)}
+              title={intl.formatMessage(messages.resetTitle, { label: optionActive.label })}
+            >
+              <ScreenReaderHide>
+                {optionActive.label}
+              </ScreenReaderHide>
               <Icon name="removeSmall" text textRight hidePrint />
             </Reset>
           )
