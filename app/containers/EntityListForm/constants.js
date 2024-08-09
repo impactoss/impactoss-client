f620e67e99b309af146d92969738327457f1dcd3
vbojilova <viki.bojilova@gmail.com>
2024-08-09 14:11:36 -0700

    added formik to entitylistform #556

--- app/containers/EntityListForm/constants.js
+++ app/containers/EntityListForm/constants.js
@@ -6,3 +6,7 @@
 export const FILTER_FORM_MODEL = 'entityListForm.forms.filterData';
 export const EDIT_FORM_MODEL = 'entityListForm.forms.editData';
 export const SET_FILTER = 'entityListForm.forms.setFilter';
+
+export const INITIAL_FORM = {
+  values: [],
+};
