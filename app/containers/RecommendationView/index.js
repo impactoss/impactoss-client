90c423db98680d8aa75608894d7afe816b4d08a9
tmfrnz <timo@dumpark.com>
2024-08-13 17:14:24 +0200

    fix default values

--- app/containers/RecommendationView/index.js
+++ app/containers/RecommendationView/index.js
@@ -94,8 +94,21 @@ export class RecommendationView extends React.PureComponent { // eslint-disable-
     {
       fields: [
         getStatusField(entity),
-        getStatusField(entity, 'is_current', IS_CURRENT_STATUSES, appMessages.attributes.is_current, 'current'),
-        getStatusField(entity, 'is_archive', IS_ARCHIVE_STATUSES, appMessages.attributes.is_archive, 'false'),
+        getStatusField(
+          entity,
+          'is_current',
+          IS_CURRENT_STATUSES,
+          appMessages.attributes.is_current,
+          true,
+        ),
+        entity.getIn(['attributes', 'is_archive'])
+          && getStatusField(
+            entity,
+            'is_archive',
+            IS_ARCHIVE_STATUSES,
+            appMessages.attributes.is_archive,
+            false,
+          ),
         getMetaField(entity),
       ],
     },
