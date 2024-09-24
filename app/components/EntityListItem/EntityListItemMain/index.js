e6fec7ddbedeeeb1248d6ffc50f552b32faf2b15
tmfrnz <timo@dumpark.com>
2024-09-24 19:35:41 +0200

    fix list item padding

--- app/components/EntityListItem/EntityListItemMain/index.js
+++ app/components/EntityListItem/EntityListItemMain/index.js
@@ -24,16 +24,23 @@ import EntityListItemMainBottom from './EntityListItemMainBottom';
 
 const Styled = styled((p) => <Component {...p} />)`
   position: relative;
-  padding-left: 0;
-  padding-left: ${(props) => !props.theme.sizes
-    ? 0
-    : props.theme.sizes.mainListItemSmall.paddingHorizontal
+  padding-left: ${({ theme }) => {
+    if (!theme.sizes) {
+      return 4;
+    }
+    return theme.sizes.mainListItemSmall.paddingHorizontal;
+  }
 }px;
   box-shadow: ${({ isConnection }) => isConnection ? '0px 0px 6px 0px rgba(0,0,0,0.2)' : 'none'};
   @media (min-width: ${(props) => props.theme && props.theme.breakpoints ? props.theme.breakpoints.small : '769px'}) {
-    padding-left: ${(props) => (!props.theme.sizes || props.isManager)
-    ? 0
-    : props.theme.sizes.mainListItemSmall.paddingHorizontal
+    padding-left: ${({ theme, isManager, isConnection }) => {
+    if (!theme.sizes) {
+      return 4;
+    }
+    return (isManager && !isConnection)
+      ? 0
+      : theme.sizes.mainListItem.paddingHorizontal;
+  }
 }px;
   }
   @media print {
@@ -278,6 +285,7 @@ class EntityListItemMain extends React.PureComponent { // eslint-disable-line re
     const entity = this.mapToEntityListItem(this.props);
 
     const bottomTaxonomies = taxonomies && taxonomies.filter((tax) => !qe(tax.get('id'), PROGRESS_TAXONOMY_ID));
+
     return (
       <Styled isManager={this.props.isManager} isConnection={this.props.isConnection}>
         <Wrapper>
