ea3b8727060628bbd2b3caaeae8abc2e41e98ef2
tmfrnz <timo@dumpark.com>
2024-12-12 18:24:49 +0100

    make sure auth token is stored and entities reloaded thereafter on auth success

--- app/containers/UserLoginOAuthSuccess/index.js
+++ app/containers/UserLoginOAuthSuccess/index.js
@@ -6,62 +6,63 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage, injectIntl, intlShape } from 'react-intl';
 import { connect } from 'react-redux';
 import HelmetCanonical from 'components/HelmetCanonical';
 
 // import { ROUTES } from 'containers/App/constants';
-import { forwardOnAuthenticationChange } from 'containers/App/actions';
+import {
+  forwardOnAuthenticationChange,
+  validateToken,
+} from 'containers/App/actions';
+
 import ContentNarrow from 'components/ContentNarrow';
 import Loading from 'components/Loading';
 
 import messages from './messages';
 
-export class UserLoginOAuthSuccess extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
-  componentDidMount() {
-    this.props.handleForward();
-  }
-
-  render() {
-    const { intl } = this.context;
-
-    return (
-      <div>
-        <HelmetCanonical
-          title={`${intl.formatMessage(messages.pageTitle)}`}
-          meta={[
-            {
-              name: 'description',
-              content: intl.formatMessage(messages.metaDescription),
-            },
-          ]}
-        />
-        <ContentNarrow>
-          <p>
-            <FormattedMessage {...messages.note} />
-          </p>
-          <Loading />
-        </ContentNarrow>
-      </div>
-    );
-  }
-}
+const UserLoginOAuthSuccess = ({
+  intl, handleForward,
+}) => {
+  React.useEffect(() => {
+    handleForward();
+  }, []);
+  return (
+    <div>
+      <HelmetCanonical
+        title={`${intl.formatMessage(messages.pageTitle)}`}
+        meta={[
+          {
+            name: 'description',
+            content: intl.formatMessage(messages.metaDescription),
+          },
+        ]}
+      />
+      <ContentNarrow>
+        <p>
+          <FormattedMessage {...messages.note} />
+        </p>
+        <Loading />
+      </ContentNarrow>
+    </div>
+  );
+};
 
 UserLoginOAuthSuccess.propTypes = {
   handleForward: PropTypes.func.isRequired,
   path: PropTypes.string,
-};
-
-UserLoginOAuthSuccess.contextTypes = {
-  intl: PropTypes.object.isRequired,
+  intl: intlShape,
 };
 
 export function mapDispatchToProps(dispatch) {
   return {
+    onInvalidateEntities: () => {
+    },
     handleForward: () => {
+      dispatch(validateToken());
       dispatch(forwardOnAuthenticationChange());
     },
   };
 }
 
-export default connect(null, mapDispatchToProps)(UserLoginOAuthSuccess);
+export default connect(null, mapDispatchToProps)(injectIntl(UserLoginOAuthSuccess));
