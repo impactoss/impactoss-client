b6594faed05257ab227a68d4c6f48d3cc9f446bf
tmfrnz <timo@dumpark.com>
2026-03-04 13:23:20 +0100

    tax icons in tags [a11y#3], also fix labels [a11y#10]

--- app/components/EntityListItem/EntityListItemMain/EntityListItemMainBottom/EntityListItemMainTaxonomies.js
+++ app/components/EntityListItem/EntityListItemMain/EntityListItemMainBottom/EntityListItemMainTaxonomies.js
@@ -10,6 +10,7 @@ import appMessages from 'containers/App/messages';
 
 import ButtonTagCategory from 'components/buttons/ButtonTagCategory';
 import ButtonTagCategoryInverse from 'components/buttons/ButtonTagCategoryInverse';
+import Icon from 'components/Icon';
 
 import { truncateText } from 'utils/string';
 import { TEXT_TRUNCATE } from 'themes/config';
@@ -20,6 +21,17 @@ const SmartGroup = styled.div`
   padding-right: 0.5em;
   border-right: ${(props) => props.border ? '1px solid' : 'none'};
   border-right-color: ${palette('light', 3)};
+`;
+const Group = styled.div`
+  display: inline-block;
+  margin-right: 2px;
+`;
+const IconWrap = styled.div`
+  display: inline-block;
+  margin-right: 2px;
+  position: relative;
+  top: -1px;
+  left: -2px;
 `;
 const Styled = styled.div`
   width: 100%;
@@ -34,13 +46,14 @@ const Styled = styled.div`
 // border-top-color:;
 
 class EntityListItemMainTaxonomies extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
-  getEntityTags = (categories, taxonomies, onClick, intl) => {
-    const tags = [];
+  getEntityTags = (categories, taxonomies, onClick) => {
+    const tagsByTax = {};
     if (categories) {
       taxonomies
         .filter((tax) => !tax.getIn(['attributes', 'is_smart']))
         .sortBy((tax) => tax.getIn(['attributes', 'priority']))
         .forEach((tax) => {
+          const taxId = tax.get('id');
           tax
             .get('categories')
             .sortBy((category) => category.getIn(['attributes', 'draft']))
@@ -49,12 +62,14 @@ class EntityListItemMainTaxonomies extends React.PureComponent { // eslint-disab
                 const label = (category.getIn(['attributes', 'short_title']) && category.getIn(['attributes', 'short_title']).trim().length > 0
                   ? category.getIn(['attributes', 'short_title'])
                   : category.getIn(['attributes', 'title']));
-                const title = `${intl.formatMessage(
-                  appMessages.entities.taxonomies[tax.get('id')].shortSingle,
-                )}: ${category.getIn(['attributes', 'title'])}`;
-                tags.push({
-                  taxId: tax.get('id'),
+                const title = category.getIn(['attributes', 'title']);
+                if (!tagsByTax[taxId]) {
+                  tagsByTax[taxId] = [];
+                }
+                tagsByTax[taxId].push({
+                  taxId,
                   title,
+                  catId: category.get('id'),
                   inverse: category.getIn(['attributes', 'draft']),
                   label: truncateText(label, TEXT_TRUNCATE.ENTITY_TAG, categories.size < 5),
                   onClick: () => onClick(catId, 'category'),
@@ -63,7 +78,7 @@ class EntityListItemMainTaxonomies extends React.PureComponent { // eslint-disab
             });
         });
     }
-    return tags;
+    return tagsByTax;
   };
 
   getSmartTitle = (title, isSmart, intl) =>
@@ -97,19 +112,22 @@ class EntityListItemMainTaxonomies extends React.PureComponent { // eslint-disab
       categories, taxonomies, onEntityClick, intl,
     } = this.props;
     const smartTaxonomy = taxonomies && taxonomies.find((tax) => tax.getIn(['attributes', 'is_smart']));
-    const entityTags = categories && this.getEntityTags(categories, taxonomies, onEntityClick, intl);
-
+    const entityTags = categories && this.getEntityTags(categories, taxonomies, onEntityClick);
     return (
-      <Styled role="group" aria-label="Categories">
+      <Styled role="group" aria-label="Category groups">
         {smartTaxonomy && (
-          <SmartGroup border={entityTags && entityTags.length > 0}>
+          <SmartGroup
+            border={entityTags && entityTags.length > 0}
+            role="group"
+            aria-label="SMART criteria"
+          >
             {this.getEntitySmartTags(categories, smartTaxonomy, onEntityClick, intl).map((tag, i) => (
               <ButtonTagCategory
                 key={i}
                 onClick={tag.onClick}
                 taxId={parseInt(tag.taxId, 10)}
                 title={tag.title}
-                aria-label={tag.title}
+                aria-label={tag.label}
                 isSmartTag
                 isSmart={tag.isSmart}
               >
@@ -118,39 +136,67 @@ class EntityListItemMainTaxonomies extends React.PureComponent { // eslint-disab
             ))}
           </SmartGroup>
         )}
-        {entityTags && entityTags.map((tag, i) => tag.inverse
-          ? (
-            <ButtonTagCategoryInverse
-              key={i}
-              onClick={tag.onClick}
-              taxId={parseInt(tag.taxId, 10)}
-              disabled={!tag.onClick}
-              title={tag.title}
-              aria-label={tag.title}
-            >
-              {tag.label}
-            </ButtonTagCategoryInverse>
-          )
-          : (
-            <ButtonTagCategory
-              key={i}
-              onClick={tag.onClick}
-              taxId={parseInt(tag.taxId, 10)}
-              disabled={!tag.onClick}
-              title={tag.title}
-              aria-label={tag.title}
-            >
-              {tag.label}
-            </ButtonTagCategory>
-          ))}
+        {entityTags && Object.keys(entityTags).map((taxId) => {
+          const tags = entityTags[taxId];
+          if (tags && tags.length > 0) {
+            return (
+              <Group
+                key={taxId}
+                role="group"
+                aria-label={intl.formatMessage(
+                  appMessages.entities.taxonomies[taxId].shortSingle,
+                )}
+              >
+                {tags.map((tag) => tag.inverse
+                  ? (
+                    <ButtonTagCategoryInverse
+                      key={tag.catId}
+                      onClick={tag.onClick}
+                      taxId={parseInt(tag.taxId, 10)}
+                      disabled={!tag.onClick}
+                      title={tag.title}
+                      aria-label={tag.label}
+                    >
+                      <IconWrap>
+                        <Icon name={`taxonomy_small_${taxId}`} size="15px" />
+                      </IconWrap>
+                      {tag.label}
+                    </ButtonTagCategoryInverse>
+                  )
+                  : (
+                    <ButtonTagCategory
+                      key={tag.catId}
+                      onClick={tag.onClick}
+                      taxId={parseInt(tag.taxId, 10)}
+                      disabled={!tag.onClick}
+                      title={tag.title}
+                      aria-label={tag.label}
+                    >
+                      <IconWrap>
+                        <Icon
+                          name={`taxonomy_small_${taxId}`}
+                          color="white"
+                          size="15px"
+                          aria-hidden="true"
+                        />
+                      </IconWrap>
+                      {tag.label}
+                    </ButtonTagCategory>
+                  ))
+                }
+              </Group>
+            );
+          }
+          return null;
+        })}
       </Styled>
     );
   }
 }
 
 EntityListItemMainTaxonomies.propTypes = {
-  categories: PropTypes.instanceOf(Map), // eslint-disable-line react/no-unused-prop-types
-  taxonomies: PropTypes.instanceOf(Map), // eslint-disable-line react/no-unused-prop-types
+  categories: PropTypes.instanceOf(Map),
+  taxonomies: PropTypes.instanceOf(Map),
   onEntityClick: PropTypes.func,
   intl: PropTypes.object.isRequired,
 };
